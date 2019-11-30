9f23606611f237f4efab04aa9901a3787ec99b8c
Jake Bowhay <jb9.bowhay@gmail.com>
2019-11-30 22:46:08 +0000

    improvements to ui

--- launcher.py
+++ launcher.py
@@ -1,4 +1,5 @@
 import tkinter
+from tkinter import messagebox
 from block_simulator import Simulation
 
 
@@ -7,16 +8,17 @@ class Application(tkinter.Frame):
         super().__init__(master)
         self.grid()
         self.NumberOfBlocks = 1
+        self.BlockRows = []
         self.create_widgets(master)
 
     def create_widgets(self, master):
         tkinter.Label(master, text='Horizontal Block Collision Simulator',
                       font=('Lucida Grande', 30)).grid(row=0, column=1, columnspan=3, sticky='we')
 
-        tkinter.Button(master, text='Add Block',command=lambda: self.generate_button_row(master, self.NumberOfBlocks))\
+        tkinter.Button(master, text='Add Block', command=lambda: self.generate_button_row(master, self.NumberOfBlocks)) \
             .grid(row=2, column=1)
 
-        tkinter.Button(master, text='Remove Block').grid(row=2, column=2)
+        tkinter.Button(master, text='Remove Block', command=lambda: self.delete_button_row()).grid(row=2, column=2)
 
         tkinter.Button(master, text='Run Simulation', command=lambda: self.launch_simulation()).grid(row=2, column=3)
 
@@ -27,11 +29,25 @@ class Application(tkinter.Frame):
             self.generate_button_row(master, row)
 
     def generate_button_row(self, master, BlockNumber):
-        RowNumber = BlockNumber + 3
-        tkinter.Label(master, text='Block {}'.format(BlockNumber)).grid(row=RowNumber, column=1, sticky='w')
-        tkinter.Entry(master).grid(row=RowNumber, column=2)
-        tkinter.Entry(master).grid(row=RowNumber, column=3)
+        Row = []
+        RowNumber = BlockNumber + 4
+        Row.append(tkinter.Label(master, text='Block {}'.format(BlockNumber)))
+        for i in range(2):
+            Row.append(tkinter.Entry(master))
+        for x, Item in enumerate(Row):
+            Item.grid(row=RowNumber, column=x + 1)
+        self.BlockRows.append(Row)
         self.NumberOfBlocks += 1
+
+    def delete_button_row(self):
+        try:
+            LastRow = self.BlockRows.pop()
+        except:
+            messagebox.showerror('Error', 'No block to delete')
+        else:
+            for Item in LastRow:
+                Item.grid_forget()
+            self.NumberOfBlocks -= 1
 
     def launch_simulation(self):
         simulation = Simulation(1280, 720, "Block Collision Simulator", resizable=False)
