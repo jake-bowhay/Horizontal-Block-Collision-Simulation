1726c27e6aa48968c0b016db58baa4053a65addc
Jake Bowhay <jb9.bowhay@gmail.com>
2019-12-01 22:01:46 +0000

    Prevent user from adding too many blocks

--- launcher.py
+++ launcher.py
@@ -7,7 +7,7 @@ class Application(tkinter.Frame):
     def __init__(self, master=None):
         super().__init__(master)
         self.grid()
-        self.NumberOfBlocks = 1
+        self.NumberOfBlocks = 0
         self.BlockRows = []
         self.create_widgets(master)
 
@@ -15,7 +15,7 @@ class Application(tkinter.Frame):
         tkinter.Label(master, text='Horizontal Block Collision Simulator',
                       font=('Lucida Grande', 30)).grid(row=0, column=1, columnspan=3, sticky='we')
 
-        tkinter.Button(master, text='Add Block', command=lambda: self.generate_button_row(master, self.NumberOfBlocks)) \
+        tkinter.Button(master, text='Add Block', command=lambda: self.generate_button_row(master))\
             .grid(row=2, column=1)
 
         tkinter.Button(master, text='Remove Block', command=lambda: self.delete_button_row()).grid(row=2, column=2)
@@ -26,18 +26,22 @@ class Application(tkinter.Frame):
         tkinter.Label(master, text='Elasticity').grid(row=3, column=3)
 
         for row in range(1, 3):
-            self.generate_button_row(master, row)
+            self.generate_button_row(master)
 
-    def generate_button_row(self, master, BlockNumber):
-        Row = []
-        RowNumber = BlockNumber + 4
-        Row.append(tkinter.Label(master, text='Block {}'.format(BlockNumber)))
-        for i in range(2):
-            Row.append(tkinter.Entry(master))
-        for x, Item in enumerate(Row):
-            Item.grid(row=RowNumber, column=x + 1)
-        self.BlockRows.append(Row)
-        self.NumberOfBlocks += 1
+    def generate_button_row(self, master):
+        print(self.NumberOfBlocks)
+        if self.NumberOfBlocks < 5:
+            self.NumberOfBlocks += 1
+            Row = []
+            RowNumber = self.NumberOfBlocks + 4
+            Row.append(tkinter.Label(master, text='Block {}'.format(self.NumberOfBlocks)))
+            for i in range(2):
+                Row.append(tkinter.Entry(master))
+            for x, Item in enumerate(Row):
+                Item.grid(row=RowNumber, column=x + 1)
+            self.BlockRows.append(Row)
+        else:
+            messagebox.showwarning('Maximum Number Exceeded', "The maximum number of blocks that can be simulated is 5")
 
     def delete_button_row(self):
         try:
